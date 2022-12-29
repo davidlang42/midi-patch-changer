fbda10e98bc90fe3d020207bc578c4ff189610c5
David Lang <davidlang42@gmail.com>
2022-12-29 14:09:55 +1100

    Basic interface

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::error::Error;
 use std::sync::mpsc;
 use std::thread;
 use std::io::{Read, Write};
+use console::{Term, Key};
+//TODO send proper midi messages
 // use wmidi::MidiMessage;
 
 #[macro_use] extern crate serde_derive;
@@ -31,12 +33,52 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut input = fs::File::options().read(true).open(midi_in).map_err(|e| format!("Cannot open MIDI IN '{}': {}", midi_in, e))?;
     let mut output = fs::File::options().write(true).open(midi_out).map_err(|e| format!("Cannot open MIDI OUT '{}': {}", midi_out, e))?;
     let (tx, rx) = mpsc::channel();
+    let tx_clone = tx.clone();
     thread::Builder::new().name(format!("midi-out")).spawn(move || write_from_queue(&mut output, rx))?;
-    thread::Builder::new().name(format!("midi-in")).spawn(move || read_into_queue(&mut input, tx.clone()))?;
+    thread::Builder::new().name(format!("midi-in")).spawn(move || read_into_queue(&mut input, tx_clone))?;
     // run patch change UI
-    loop {
-        //TODO
+    let term = Term::stdout();
+    let mut p = 0;
+    if patches.len() == 0 {
+        println!("**NO PATCHES**");
+    } else {
+        println!("{}", send_patch(&patches, p, &tx));
     }
+    while let Ok(k) = term.read_key() {
+        match k {
+            _ if patches.len() == 0 => {
+                println!("**NO PATCHES**");
+            },
+            Key::Backspace if p == 0 => {
+                println!("**FIRST PATCH**");
+            },
+            Key::Backspace => {
+                p -= 1;
+                println!("<<< {}", send_patch(&patches, p, &tx));
+            },
+            _ if p == patches.len() - 1 => {
+                println!("**LAST PATCH**");
+            },
+            _ => {
+                p += 1;
+                println!("{}", send_patch(&patches, p, &tx));
+            }
+        };
+    }
+    Ok(())
+}
+
+fn send_patch(patches: &Vec<PatchPreset>, index: usize, tx: &mpsc::Sender<u8>) -> String {
+    let patch = &patches[index];
+    if let Some(number) = patch.patch {
+        if tx.send(192).is_err() {
+            return format!("Error sending patch change #{}", index + 1);
+        }
+        if tx.send(number).is_err() {
+            return format!("Error sending patch change #{}", index + 1);
+        }
+    }
+    format!("#{} {}", index + 1, patch.name)
 }
 
 fn read_into_queue(f: &mut fs::File, tx: mpsc::Sender<u8>) {
